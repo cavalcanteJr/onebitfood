20416ef8f7cb6d2a2af9d4aaff9657a105959f2a
Lourival <cavalcantebsi@gmail.com>
2019-05-01 13:19:45 -0400

    Correção do serializer

---
+++ .history/app/controllers/restaurants_controller_20190430205834.rb
@@ -0,0 +1,23 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: :show
+  def index
+    @restaurants = Restaurant.near(params[:city] || 'São Paulo')
+  filter_by_category if params[:category]
+  render json: @restaurants
+  end
+
+  def show
+    
+  end
+
+  def search
+   
+  end
+
+  private
+  def filter_by_category
+    @restaurants = @restaurants.select do |r|
+      r.category.title == params[:category]
+    end
+  end   
+end
